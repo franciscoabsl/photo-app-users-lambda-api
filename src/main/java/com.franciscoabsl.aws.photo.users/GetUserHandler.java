607a20c695112b489a8ee70fbb91d30c5a7cdeea
franciscoabsl <franciscosouzalima@gmail.com>
2024-05-29 20:16:22 -0300

    add GetUserHandler and implement handleRequest with APIGatewayProxy events

---
+++ src/main/java/com.franciscoabsl.aws.photo.users/GetUserHandler.java
@@ -0,0 +1,30 @@
+package com.franciscoabsl.aws.photo.users;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.JsonObject;
+
+import java.util.Map;
+
+public class GetUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+
+        Map<String, String> pathParameters =  input.getPathParameters();
+        String userId = pathParameters.get("userId");
+
+        JsonObject returnValue = new JsonObject();
+        returnValue.addProperty("firstName","Francisco");
+        returnValue.addProperty("lastName","Lima");
+        returnValue.addProperty("id",userId);
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.withStatusCode(200).withBody(returnValue.toString());
+
+        return response;
+    }
+
+}
